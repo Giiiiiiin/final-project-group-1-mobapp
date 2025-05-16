dde4987709e339c321409c323828a7cbc295e9fd
KurtPaulino <kurtlylep@gmail.com>
2025-05-16 22:54:56 +0800

    Test Commit

--- final-proj-gr1-mobapp/App.tsx
+++ final-proj-gr1-mobapp/App.tsx
@@ -1,5 +1,6 @@
 // TEST AAAAAAAAAAAAA
 // TEST AAAAAAAAAAAAA
+// TEST AAAAAAAAAAAAA
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
